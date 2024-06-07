c66861141babbcf22140e7b603c9ee91bd4b72df
NoahBYRokko <seniorsova12@gmail.com>
2024-06-07 16:56:16 +0400

    third commit

---
+++ Level 08/Classwork/classwork.py
@@ -0,0 +1,16 @@
+# Task1.
+
+num1 = int(input("Please enter your age: "))  
+print(18 <= num1 <= 25) 
+
+# Task2.
+
+num1 = int(input("Please enter your budget: "))
+print(50 >= num1 or num1 >= 100)
+
+
+# Task3.
+
+for i in range(10):
+    print("Bachi")
+      
---
+++ Level 08/Homework/homework.py
@@ -0,0 +1,72 @@
+ # Task1.
+
+num1 = int(input("Please enter first number: "))
+num2 = int(input("Please enter second number: "))
+num3 = int(input("Please enter third number: "))
+num4 = int(input("Please enter forth number: "))
+num5 = int(input("Please enter fifth number: "))
+
+print(num1 + num2 + num3 + num4 + num5)
+print(num1 - num2 - num3 - num4 - num5)
+print(num1 * num2 * num3 * num4 * num5)
+print(num1 / num2 / num3 / num4 / num5)
+print(num1 % num2 % num3 % num4 % num5)
+print(num1 // num2 // num3 // num4 // num5)
+
+# Task2.
+
+age = int(input("Please enter your age: "))
+print(18 < age < 20)
+
+# Task3.  
+print(10 < 20)
+print(20 < 30)
+print(30 < 40)
+print(40 < 50)
+print(50 < 60)
+
+print(60 > 50)
+print(50 > 40)
+print(40 > 30)
+print(30 > 20)
+print(20 > 10)
+
+print(10 >= 10)
+print(20 >= 10)
+print(30 >= 30)
+print(40 >= 20)
+print(50 >= 10)
+
+print(20 <= 60)
+print(10 <= 10)
+print(30 <= 40)
+print(50 <= 50)
+print(10 <= 60)
+
+print(20 != 60)
+print(10 != 10)
+print(30 != 40)
+print(50 != 50)
+print(10 != 60)
+
+print(10 == 10)
+print(20 == 20)
+print(30 == 30)
+print(40 == 40)
+print(50 == 50)
+
+# Task4.  [Unfinished]
+
+num1 = int(input("Please enter integer number: "))
+num2 = float(input("Please enter float number: "))
+
+print(num1 != num2)
+
+# Task5.
+num1 = 20
+num2 = 15.5
+
+print(type(num1))
+print(type(num2))
+
+
