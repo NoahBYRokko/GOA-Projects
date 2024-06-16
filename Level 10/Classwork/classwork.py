6082e60755d1e85848806d9fb311e829c3e0abac
NoahBYRokko <seniorsova12@gmail.com>
2024-06-16 13:41:57 +0400

    fifth commit

---
+++ Level 10/Classwork/classwork.py
@@ -0,0 +1,24 @@
+# Task1.
+
+num1 = input("Please enter your password: ")
+if num1 == "luka1234":
+    print("You have successfully logged in.")
+else:
+    print("Your Password was incorrect.")
+
+# Task2.
+
+product = 1
+for i in range(5):
+    num = int(input("Please enter number: "))
+    product = product * num
+
+print(product)
+
+# Task3.
+
+total = 0
+for i in range(5, 17):
+    total += i
+
+print(total)
