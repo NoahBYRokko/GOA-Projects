2b2889cacf480d1c6816c3c68b66a8995340c887
NoahBYRokko <seniorsova12@gmail.com>
2024-07-17 20:51:19 +0400

    eighteenth commit

---
+++ Level 17/Classwork/classwork.py
@@ -0,0 +1,16 @@
+# Task1.
+
+print("__________________Task1__________________")
+
+car = {
+    "mark": "Honda",
+    "name": "Civic",
+    "year": "2015",
+    "engine": "1.8L i-VTEC 4-cylinder",
+}
+
+
+
+
+
+
